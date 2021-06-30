95905d946e92df0aaaa4a9175339ca45442f1b7e
Florin Lazar <florinlazar27@gmail.com>
2021-06-30 21:38:22 +0300

    Print the args using Debug/Display traits

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 
 use structopt::StructOpt;
 
+use std::fmt::{Display, Formatter, Result};
+
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
+#[derive(Debug)]
 struct Cli {
     /// The pattern to look for.
     pattern: String,
@@ -12,6 +15,20 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+impl Display for Cli {
+    fn fmt(&self, formatter: &mut Formatter) -> Result {
+        write!(formatter, "This is the pattern: {} and this is the path: {:?}",
+            self.pattern, self.path)
+    }
+}
+
 fn main() {
     let args = Cli::from_args();
+
+    println!("Pattern: {}", args.pattern);
+    println!("Path (debug form): {:?}", args.path);
+
+    println!("Cli args struct (debug): {:?}", args);
+
+    println!("Cli args struct (display): {}", args)
 }
