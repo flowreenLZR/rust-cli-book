7ddbb20d754b29937a726d183a4329b2b4ffa97d
Florin Lazar <florinlazar27@gmail.com>
2021-07-03 23:03:46 +0300

    Implement CustomError to provide detailed context

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,20 @@ impl Display for Cli {
     }
 }
 
+// Simple struct with one member.
+#[derive(Debug)]
+struct CustomError(String);
+
 // [^Option 5/6]
 // fn main() {
+
 // Option 5/6.
 // "Box" is some kind of "unique_ptr" from C++?
-fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+// fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+
+// Option 7.
+// @todo Why isn't "Box" needed anymore?
+fn main() -> std::result::Result<(), CustomError> {
     // Use the *from_args* method provided by *derive(StructOpt)"
     // to parse the input arguments.
     let args = Cli::from_args();
@@ -62,6 +71,7 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     // "File::open" returns a "Result" which can be evaluated using "expect".
     // let file = file.expect("File could not be opened!");
     // let buf_reader = std::io::BufReader::new(file);
+
     // Option 2.
     // It also can be evaluated using a match.
     // let buf_reader : std::io::BufReader<std::fs::File>;
@@ -77,6 +87,7 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     //                 // @todo Is there a better way to halt execution?
     //     }
     // }
+
     // Option 3.
     // Just like option 2 but instead of print and return, panic.
     // let buf_reader = match file {
@@ -88,10 +99,12 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     //         panic!("File was not opened properly, error is: {}", msg);
     //     }
     // };
+
     // Option 4.
     // Use "Result::unwrap". Also panics.
     // Shortcut for option 3.
     // let buf_reader = std::io::BufReader::new(file.unwrap());
+
     // Option 5.
     // Just like option 2 but with nicer return. This changes signature of main
     // and the return value.
@@ -99,15 +112,34 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     //     Ok(file) => { std::io::BufReader::new(file) },
     //     Err(msg) => { return Err(msg.into()); }
     // };
+
     // Option 6.
     // Shortcut for option 5.
     // Although File::open may return a std::io::Error and main returns std::error::Error,
     // "?" expands to code that converts between error types. Kind of what the "Err" branch does
     // in "Option 5"?
-    let buf_reader = std::io::BufReader::new(file?);
+    // let buf_reader = std::io::BufReader::new(file?);
+
+    // Option 7.
+    // Use a custom, user-defined error to provide a much more specific message.
+    // All "?" operators must be prefixed with the "map_err" call.
+    // That is unless the custom error implements the "From<"Error_Type">" trait where "Error_Type"
+    // is the error type "?" was handling before. This is because "?" expands to code that does
+    // error conversions as long as the necessary "From<E>" traits.
+    let file = file.map_err(|err| CustomError(
+        format!("Error reading `{:?}`: {}", args.path, err)
+    ))?;
+    let buf_reader = std::io::BufReader::new(file);
 
     for line in buf_reader.lines() {
-        let line = line?;
+        // ^Option7
+        // let line = line?;
+
+        // Option 7.
+        // Either this or implement "From<std::io::Error>" for "CustomError".
+        let line = line.map_err(|_| CustomError(
+            format!("Could not read line from file!")
+        ))?;
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
