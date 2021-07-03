e94c2784230a8619c3040613e86fde9155106c73
Florin Lazar <florinlazar27@gmail.com>
2021-07-03 17:00:23 +0300

    Search the input file to find the pattern

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,8 @@ impl Display for Cli {
 }
 
 fn main() {
+    // Use the *from_args* method provided by *derive(StructOpt)"
+    // to parse the input arguments.
     let args = Cli::from_args();
 
     println!("Pattern: {}", args.pattern);
@@ -30,5 +32,19 @@ fn main() {
 
     println!("Cli args struct (debug): {:?}", args);
 
-    println!("Cli args struct (display): {}", args)
+    println!("Cli args struct (display): {}", args);
+
+    /// "read_to_string" returns a "Result" struct which may contain
+    /// an OK value or an Err value.
+    /// "expect" then consumes the "self" object, returning the OK value
+    /// or panicking if the the result is an "Err".
+    let content = std::fs::read_to_string(&args.path)
+        .expect("Could not read the file!");
+
+    /// Iterate over content and match pattern.
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
