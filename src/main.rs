072bdb4ac5c8a10f362d99065e0c2571bf235012
Florin Lazar <florinlazar27@gmail.com>
2021-07-03 22:24:12 +0300

    Implement multiple ways of dealing with errors

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,11 @@ impl Display for Cli {
     }
 }
 
-fn main() {
+// [^Option 5/6]
+// fn main() {
+// Option 5/6.
+// "Box" is some kind of "unique_ptr" from C++?
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     // Use the *from_args* method provided by *derive(StructOpt)"
     // to parse the input arguments.
     let args = Cli::from_args();
@@ -39,28 +43,76 @@ fn main() {
     // an OK value or an Err value.
     // "expect" then consumes the "self" object, returning the OK value
     // or panicking if the the result is an "Err".
-    let content = std::fs::read_to_string(&args.path)
-        .expect("Could not read the file!");
+    // let content = std::fs::read_to_string(&args.path)
+    //     .expect("Could not read the file!");
 
     // Iterate over content and match pattern.
-    for line in content.lines() {
+    // for line in content.lines() {
+    //     if line.contains(&args.pattern) {
+    //         println!("{}", line);
+    //     }
+    // }
+
+    // Using std::fs::read_to_string is memory expensive because it reads the whole file
+    // into memory.
+    // BufReader should solve that.
+
+    let file = std::fs::File::open(&args.path);
+    // Option 1.
+    // "File::open" returns a "Result" which can be evaluated using "expect".
+    // let file = file.expect("File could not be opened!");
+    // let buf_reader = std::io::BufReader::new(file);
+    // Option 2.
+    // It also can be evaluated using a match.
+    // let buf_reader : std::io::BufReader<std::fs::File>;
+    // match file {
+    //     Ok(handle) => {
+    //         buf_reader = std::io::BufReader::new(handle);
+    //         println!("File was opened properly!")
+    //     },
+    //     Err(msg) => {
+    //         println!("File was not opened properly, error is: {}", msg);
+    //         return; // This was needed otherwise rustc would report that
+    //                 // buf_reader might be used uninitialized.
+    //                 // @todo Is there a better way to halt execution?
+    //     }
+    // }
+    // Option 3.
+    // Just like option 2 but instead of print and return, panic.
+    // let buf_reader = match file {
+    //     Ok(handle) => {
+    //         println!("File was opened properly!");
+    //         std::io::BufReader::new(handle)
+    //     },
+    //     Err(msg) => {
+    //         panic!("File was not opened properly, error is: {}", msg);
+    //     }
+    // };
+    // Option 4.
+    // Use "Result::unwrap". Also panics.
+    // Shortcut for option 3.
+    // let buf_reader = std::io::BufReader::new(file.unwrap());
+    // Option 5.
+    // Just like option 2 but with nicer return. This changes signature of main
+    // and the return value.
+    // let buf_reader = match file {
+    //     Ok(file) => { std::io::BufReader::new(file) },
+    //     Err(msg) => { return Err(msg.into()); }
+    // };
+    // Option 6.
+    // Shortcut for option 5.
+    // Although File::open may return a std::io::Error and main returns std::error::Error,
+    // "?" expands to code that converts between error types. Kind of what the "Err" branch does
+    // in "Option 5"?
+    let buf_reader = std::io::BufReader::new(file?);
+
+    for line in buf_reader.lines() {
+        let line = line?;
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
 
-    // Using std::fs::read_to_string is memory expensive because it reads the whole file
-    // into memory.
-    // BufReader should solve that.
-    println!("Doing the same thing only this time using BufReader.");
-
-    let file = std::fs::File::open(&args.path).expect("File could not pe opened!");
-    let buf_reader = std::io::BufReader::new(file);
-
-    for line in buf_reader.lines() {
-        let string = line.expect("Iter invalid");
-        if string.contains(&args.pattern) {
-            println!("{}", string);
-        }
-    }
+    // Required for Option 5/6.
+    Ok(())
 }
